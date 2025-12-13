37e00b53c621e88e1bd2388363e69bcde26bc6b4
NZ <nzquantfin@google.com>
2025-12-13 16:37:25 +0000

    Update

--- train_gpt2_my.py
+++ train_gpt2_my.py
@@ -94,7 +94,7 @@ class GPT(nn.Module):
     ))  
     self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
 
-  def forward(self, idx):
+  def forward(self, idx, targets=None):
     B, T = idx.size()
     assert T <= self.config.block_size, f"Cannot forward sequence of length {T}, block size"
     pos = torch.arange(0, T, dtype=torch.long, device=idx.device)
@@ -109,7 +109,10 @@ class GPT(nn.Module):
     x = self.transformer.ln_f(x)
     # print(f"after ln_f: {x[0]=}")
     logits = self.lm_head(x)
-    return logits
+    loss = None
+    if targets is not None:
+        loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1))
+    return logits, loss
 
 
   @classmethod
@@ -164,14 +167,41 @@ print(f"using device: {device}")
 
 num_return_sequences = 5
 max_length = 30
+
+# device = "cpu"
+import tiktoken
+enc = tiktoken.get_encoding('gpt2')
+with open('input.txt', 'r') as f:
+  text = f.read()
+text = text[:1000]
+tokens = enc.encode(text)
+B, T = 4, 32
+buf = torch.tensor(tokens[:B*T+1])
+buf = buf.to(device)
+x = buf[:-1].view(B, T)
+y = buf[1:].view(B, T)
+
 # model = GPT.from_pretrained('gpt2')
 model = GPT(GPTConfigs())
 model.eval()
 model.to(device)
+# logits, loss = model(x,y)
 
-import tiktoken
-enc = tiktoken.get_encoding('gpt2')
-tokens = enc.encode("Hello, I'm a language model,")
+
+optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
+for i in range(50):
+  optimizer.zero_grad()
+  logits, loss = model(x, y)
+  loss.backward()
+  optimizer.step()
+  print(f"step {i}, loss: {loss.item()}")
+
+print(loss, logits.shape)
+import sys; sys.exit(0)
+
+
+
+# tokens = enc.encode("Hello, I'm a language model,")
 print(len(tokens), tokens)
 tokens = torch.tensor(tokens, dtype=torch.long)
 tokens = tokens.unsqueeze(0).repeat(num_return_sequences, 1)

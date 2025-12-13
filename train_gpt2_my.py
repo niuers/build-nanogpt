8cb720f519c57b1de9ebb0ccef28eda84463b28f
NZ <nzquantfin@google.com>
2025-12-13 13:54:37 +0000

    Update

--- train_gpt2_my.py
+++ train_gpt2_my.py
@@ -84,6 +84,20 @@ class GPT(nn.Module):
     ))  
     self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
 
+  def forward(self, idx):
+    B, T = idx.size()
+    assert T <= self.config.block_size, f"Cannot forward sequence of length {T}, block size"
+    pos = torch.arange(0, T, dtype=torch.long, device=idx.device)
+    pos_emb = self.transformer.wpe(pos)
+    tok_emb = self.transformer.wte(idx)
+    x = pos_emb + tok_emb
+    for block in self.transformer.h:
+      x = block(x)
+    
+    x = self.transformer.ln_f(x)
+    logits = self.lm_head(x)
+    return logits
+
 
   @classmethod
   def from_pretrained(cls, model_type):
@@ -128,7 +142,38 @@ class GPT(nn.Module):
     return model
 
 
+
+num_return_sequences = 5
+max_length = 30
 model = GPT.from_pretrained('gpt2')
+model.eval()
+model.to('cuda')
+
+import tiktoken
+enc = tiktoken.get_encoding('gpt2')
+tokens = enc.encode("Hello, I'm a language model,")
+print(len(tokens), tokens)
+tokens = torch.tensor(tokens, dtype=torch.long)
+tokens = tokens.unsqueeze(0).repeat(num_return_sequences, 1)
+x = tokens.to('cuda')
+
+torch.manual_seed(42)
+torch.cuda.manual_seed(42)
+while x.size(1) < max_length:
+  with torch.no_grad():
+    logits = model(x)
+    logits = logits[:, -1, :]
+    probs = F.softmax(logits, dim=-1)
+    topk_probs, topk_indices = torch.topk(probs, 50, dim=-1)
+    idx = torch.multinomial(topk_probs, 1)
+    xcol = torch.gather(topk_indices, -1, ix)
+    x = torch.cat((x, xcol), dim=1)
+  
+for i in range(num_return_sequences):
+  tokens = x[i, :max_length].tolist()
+  decoded = enc.decode(tokens)
+  print(">", decoded)
+
 print("didn't crash")
 
 

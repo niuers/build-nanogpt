d2401e853448fe427dbfc75d6ef35f85a76a74f7
NZ <nzquantfin@google.com>
2025-12-13 13:13:38 +0000

    Update

--- train_gpt2_my.py
+++ train_gpt2_my.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
+import math
 
 
 @dataclass
@@ -12,6 +13,61 @@ class GPTConfigs:
   n_head: int = 6
   n_embd: int = 384
 
+class CausalSelfAttention(nn.Module):
+  def __init__(self, config):
+    super().__init__()
+    assert config.n_embd % config.n_head == 0
+
+    self.c_attn = nn.Linear(config.n_embd, 3*config.n_embd)
+    self.c_proj = nn.Linear(config.n_embd, config.n_embd)
+    self.n_head = config.n_head
+    self.n_embd = config.n_embdself.register_buffer("bias", 
+                  torch.tril(torch.ones(config.block_size, config.block_size))
+                  .view(1,1,config.block_size, config.block_size))
+    
+  
+  def forward(self, x):
+    B, T, C = x.size()
+
+    qkv = self.c_attn(x)
+    q,k,v = qkv.split(self.n_embd, dim=2)
+    k = k.view(B, T, self.n_head, C//self.n_head).transpose(1,2)
+    q = q.view(B, T, self.n_head, C//self.n_head).transpose(1,2)
+    v = v.view(B, T, self.n_head, C//self.n_head).transpose(1,2)
+
+    att = (q @ k.transpose(-2, -1)) * (1.0/math.sqrt(k.size(-1)))
+    att = att.masked_fill(self.bias[:, :, :T, :T] == 0, float('-inf'))
+    y = att @ v
+    y = y.transpose(1,2).contiguous().view(B,T,C)
+    y = self.c_proj(y)
+    return y
+class MLP(nn.Module):
+
+  def __init__(self, config):
+    super().__init__()
+    self.c_fc = nn.Linear(config.n_embd, 4*config.n_embd)
+    self.gelu = nn.GELU(approximate='tanh')
+    self.c_proj = nn.Linear(4*config.n_embd, config.n_embd)
+  
+  def forward(self, x):
+    x = self.c_fc(x)
+    x = self.gelu(x)
+    x = self.c_proj(x)
+    return x
+
+class Block(nn.Module):
+  def __init__(self, config):
+    super().__init__()
+    self.ln_1 = nn.LayerNorm(config.n_embd)
+    # attention is communication operation
+    self.attn = CausalSelfAttention(config)
+    self.ln_2 = nn.LayerNorm(config.n_embd)
+    self.mlp = MLP(config)
+  
+  def forward(self, x):
+    x = x + self.attn(self.ln_1(x))
+    x = x + self.mlp(self.ln_2(x))
+    return x
 
 class GPT(nn.Module):
 
@@ -27,5 +83,5 @@ class GPT(nn.Module):
     ))  
     self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
 
-    
+
 
